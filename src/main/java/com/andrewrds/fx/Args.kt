b60a326b0ca6eacdd10b0555a79b3431e303085a
Andrew Dingwall-Smith <andrew@dingwall-smith.com>
2016-10-15 22:53:35 +0100

    Explicitly define type as non-null

--- src/main/java/com/andrewrds/fx/Args.kt
+++ src/main/java/com/andrewrds/fx/Args.kt
@@ -6,7 +6,7 @@ import java.math.BigDecimal
  * Command line arguments
  */
 internal class Args(args: Array<String>) {
-    val amount = if (args.size == 3) BigDecimal(args[0]) else BigDecimal.ONE
+    val amount: BigDecimal = if (args.size == 3) BigDecimal(args[0]) else BigDecimal.ONE
     val ccy1 = args[args.size - 2]
     val ccy2 = args.last()
 
