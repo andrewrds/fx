cef2e79f953e3a180f8f01ebc07d53797b857fc4
Andrew Dingwall-Smith <andrew@dingwall-smith.com>
2016-10-19 22:51:14 +0100

    Print error message if missing args

--- src/main/java/com/andrewrds/fx/Args.kt
+++ src/main/java/com/andrewrds/fx/Args.kt
@@ -1,16 +1,20 @@
 package com.andrewrds.fx
 
 import java.math.BigDecimal
+import kotlin.system.exitProcess
 
 /**
  * Command line arguments
  */
 internal class Args(args: Array<String>) {
     val amount: BigDecimal = if (args.size == 3) BigDecimal(args[0]) else BigDecimal.ONE
-    val ccy1 = args[args.size - 2]
-    val ccy2 = args.last()
+    val ccy1 = if (args.size >= 2) args[args.size - 2] else ""
+    val ccy2 = if (args.size >= 2) args.last() else ""
 
     init {
-
+        if (args.size < 2) {
+            System.err.println("Expected parameters [amount] currency1 currency2")
+            exitProcess(-1)
+        }
     }
 }
